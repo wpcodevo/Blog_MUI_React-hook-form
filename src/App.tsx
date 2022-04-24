e614d7306cc283005513793181d8b9859b3a2fb0
Edem <ziddah.edem@gmail.com>
2022-04-24 07:20:15 +0000

    image upload complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { Box, Container, CssBaseline } from '@mui/material';
+import { Box, Container, CssBaseline, Stack, Typography } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { FormProvider, useForm } from 'react-hook-form';
+import FileUpload from './components/FileUpload';
 import theme from './theme';
 
 function App() {
+  const methods = useForm();
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -15,8 +18,32 @@ function App() {
             height: '100vh',
           }}
         >
-          {/*  */}
-          <p>Hello Image Upload</p>
+          <Box display='flex' flexDirection='column' sx={{ width: '30%' }}>
+            {/* Single Image Upload */}
+            <FormProvider {...methods}>
+              <Stack marginBottom={2}>
+                <Typography
+                  textAlign='center'
+                  variant='h4'
+                  component='h1'
+                  gutterBottom
+                >
+                  Single Image Upload
+                </Typography>
+                <FileUpload limit={1} multiple={false} name='imageCover' />
+              </Stack>
+              {/* Multiple Image Upload */}
+              <Typography
+                textAlign='center'
+                variant='h4'
+                component='h1'
+                gutterBottom
+              >
+                Multiple Image Upload
+              </Typography>
+              <FileUpload limit={3} multiple name='images' />
+            </FormProvider>
+          </Box>
         </Box>
       </Container>
     </ThemeProvider>
